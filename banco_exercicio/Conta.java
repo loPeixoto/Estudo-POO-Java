79ad95b7c90ffd1e61faf2ef4c2fa47ba80d5db9
Lorenzo Peixoto Almeida <lo.peixoto@gmail.com>
2023-03-03 14:09:49 +0000

    Criando objetos

---
+++ banco_exercicio/Conta.java
@@ -0,0 +1,8 @@
+package banco_exercicio;
+
+public class Conta {
+    String numero;
+    Pessoa titular;
+    double saldo;
+    double limite;
+}
---
+++ banco_exercicio/Pessoa.java
@@ -0,0 +1,8 @@
+package banco_exercicio;
+
+public class Pessoa {
+    String nome;
+    int idade;
+    char sexo;
+    String CPF;
+}
